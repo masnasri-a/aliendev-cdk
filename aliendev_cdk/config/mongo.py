cd52c93b2cdc3ea226f4e77b409f75d3c3884404
Nasri Adzlani <nasri@jkt1.ebdesk.com>
2023-04-22 21:55:58 +0700

    :recycle: hardcode

--- aliendev_cdk/config/mongo.py
+++ aliendev_cdk/config/mongo.py
@@ -1,14 +1,14 @@
 import pymongo
-import configparser
+# import configparser
 
 def connect():
     # Membaca konfigurasi dari file config.ini
-    config = configparser.ConfigParser()
-    config.read("config.ini")
+    # config = configparser.ConfigParser()
+    # config.read("config.ini")
 
     # Mendapatkan konfigurasi koneksi ke MongoDB dari file config.ini
-    mongodb_uri = config.get("MONGODB", "uri")
-    mongodb_database = config.get("MONGODB", "database")
+    mongodb_uri = 'mongodb://root:UtyCantik12@203.194.113.203:27017'
+    mongodb_database = 'alienpdev'
 
     # Membuat koneksi ke MongoDB
     client = pymongo.MongoClient(mongodb_uri)
